b58eabd4dd3f32e78f72cf60c6a7b8d024b95200
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:24:00 +0200

    Add breadcrumbs partial

---
+++ mf-wordpress/wp-content/themes/mftheme/partials/content-breadcrumbs.php
@@ -0,0 +1,6 @@
+<div class="section-wrapper section-wrapper--hidde section-wrapper--less-pad-top-btm section-wrapper--almost-white">
+    <div class="breadcrumbs-section" typeof="BreadcrumbList" vocab="http://schema.org/">
+        <?php if(function_exists('bcn_display'))
+        { bcn_display(); }?>
+    </div>
+</div>
