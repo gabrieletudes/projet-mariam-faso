ba0ab8c6c166222a069895e06c04f5063c8f529c
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:38:05 +0200

    Add Agenda Page

--- mf-wordpress/wp-content/themes/mftheme/agenda.php
+++ mf-wordpress/wp-content/themes/mftheme/agenda.php
@@ -4,7 +4,7 @@
 */
 get_header();
 ?>
-<div class="section-wrapper">
+<div class="section-wrapper section-wrapper--almost-white">
     <section class="section section--mb-16">
         <div class="section__header section__header--to-start section__header--no-marg">
             <h2 class="section__title section__title--to-left"><?php nop_the_field('currentpage_title'); ?></h2>
@@ -14,8 +14,8 @@ get_header();
         </div>
     </section>
     <div class="button-wrapper button-wrapper--top button-wrapper--on-inversed">
-        <a href="<?= mf_get_page_url('archive-event.php');?>" class="cta-button  cta-button--mr-32 cta-button--no-mt">Voir nos derniers événements</a>
-        <a href="<?= mf_get_page_url('archive-trip.php');?>" class="cta-button">Voir nos derniers voyages</a>
+        <a href="<?= mf_get_page_url('archive-event.php');?>" class="cta-button  cta-button--mr-32 cta-button--no-mt"><?=__('Voir nos derniers événements','mf')?></a>
+        <a href="<?= mf_get_page_url('archive-trip.php');?>" class="cta-button"><?= __('Voir nos derniers voyages','mf');?></a>
     </div>
 </div>
 <!--A few images from a trip-->
