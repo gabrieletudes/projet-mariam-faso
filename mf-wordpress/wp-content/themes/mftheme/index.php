7844472ed53fc8e06e30d1a5da9bcd97ad5c7b58
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:34:58 +0200

    Add home page

--- mf-wordpress/wp-content/themes/mftheme/index.php
+++ mf-wordpress/wp-content/themes/mftheme/index.php
@@ -1,1 +1,15 @@
-Home sweet home
+<?php get_header(); ?>
+<!--Home Introduction-->
+<?php get_template_part('partials/content','homeintro'); ?>
+<!--A few projects-->
+<?php get_template_part('partials/content','fewprojects')?>
+<!--A few images from a trip-->
+<?php get_template_part('partials/content','fewgallery')?>
+<!--A few events from the events page-->
+<?php get_template_part('partials/content','fewevents')?>
+<!--CTA Trip-->
+<?php get_template_part('partials/content','ctatrip')?>
+<!--A few News articles-->
+<?php get_template_part('partials/content','fewnews')?>
+
+<?php get_footer(); ?>
