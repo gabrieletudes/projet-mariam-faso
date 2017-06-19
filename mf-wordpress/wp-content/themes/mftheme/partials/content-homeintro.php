bb19343a31fc5d16f98a16fad90fd7b013a7390d
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:26:48 +0200

    Add home intro partial

---
+++ mf-wordpress/wp-content/themes/mftheme/partials/content-homeintro.php
@@ -0,0 +1,5 @@
+<div class="section-wrapper section-wrapper--soft-orange">
+    <div class="introduction-section">
+        <h3 class="introduction-section__title"><?php nop_the_field('home_page_introduction')?></h3>
+    </div>
+</div>
