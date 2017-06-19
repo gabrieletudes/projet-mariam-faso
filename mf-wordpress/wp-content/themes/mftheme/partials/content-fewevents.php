a3fe728a315cc51a0ea171fe77798d9a37a667bd
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:25:05 +0200

    Add events partial

---
+++ mf-wordpress/wp-content/themes/mftheme/partials/content-fewevents.php
@@ -0,0 +1,35 @@
+<?php
+$args = [
+    'posts_per_page'	=> 3,
+    'post_type'			=> 'event'
+];
+
+// query
+$the_query = new WP_Query($args);
+
+?>
+<?php if( $the_query->have_posts() ): ?>
+    <div class="section-wrapper section-wrapper--very-light-gray">
+        <section class="section events-section">
+            <header class="section__header">
+                <h2 class="section__title"><?= __('Nos plus recent événements','mf');?></h2>
+            </header>
+            <?php while( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                <article class="events-section__article">
+                    <time datetime="2017-11-03" class="events-section__article-time"><?php the_field('event_start'); ?></time>
+                    <a href="<?php the_permalink();?>">
+                        <?= get_the_post_thumbnail( $post_id, 'events-small' );?>
+                    </a>
+                    <h3 class="events-section__article-title"><?php the_field('event_title'); ?></h3>
+                    <p class="events-section__article-content"><?php mf_the_excerpt(100,'event_description'); ?></p>
+                    <a href="<?php the_permalink() ?>" class="view-more-link"><?=__('Lire plus sur l’événement ','mf');?><span class="hidden"><?=the_field('event_title');?></span></a>
+                </article>
+            <?php endwhile; ?>
+        </section>
+        <div class="button-wrapper">
+            <a href="<?= mf_get_page_url('archive-event.php');?>" class="cta-button "><?= __('Voir les autres événements');?></a>
+        </div>
+    </div>
+<?php endif; ?>
+
+<?php wp_reset_query();	 // Restore global post data stomped by the_post(). ?>
