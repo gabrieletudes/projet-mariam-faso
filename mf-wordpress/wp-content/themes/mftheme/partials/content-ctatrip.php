fe06923869ee42d9b44bbc5620c68f34bc9473a1
Gabriel M <gabrieletudes@gmail.com>
2017-06-19 14:24:27 +0200

    Add ctatrip partial

---
+++ mf-wordpress/wp-content/themes/mftheme/partials/content-ctatrip.php
@@ -0,0 +1,6 @@
+<div class="cta-wrapper" style="background:linear-gradient(90deg,rgba(0,0,0,.73) 33%,rgba(0,0,0,.30) 100%),url(<?php the_field('header_image', mf_get_page_id('archive-trip.php'));?>) no-repeat center center;-webkit-background-size:cover;-moz-background-size:cover;-o-background-size:cover;background-size:cover;max-width: 100%;">
+    <div class="cta-section">
+        <h2 class="cta-section__title"><?= __('Il y a un aventurier en chacun de nous, alors ', 'mf');?><a href="<?= mf_get_page_url('archive-trip.php');?>"><?= __('envole-toi avec nous.','mf')?></a>
+        </h2>
+    </div>
+</div>
